c923db8234f28859ad4691add083f1fbd7918354
julie <julie.bouaye@hotmail.fr>
2019-04-08 15:05:09 +0200

    Ajout JUnit

--- src/test/java/dev/demo_tests_unitaires/MaClasseTest.java
+++ src/test/java/dev/demo_tests_unitaires/MaClasseTest.java
@@ -7,7 +7,6 @@ import utils.StringUtils;
 
 public class MaClasseTest {
 
-	@Test
 	public void testPremiereMethode() {
 		// On implémente un premier truc
 		try {
@@ -16,6 +15,67 @@ public class MaClasseTest {
 			CharSequence deuxiemeTruc = null;
 			Integer valeurObtenue = onTeste.levenshteinDistance(premierTruc, deuxiemeTruc);
 			Assert.assertNotNull(valeurObtenue);
+
+		} catch (NullPointerException e) {
+			System.err.println("Attention valeur nulle");
+		}
+
+		// Assert.assertEquals(2, valeurObtenue.intValue());
+
+		// Assert.assertNull(valeurObtenue);
+
+	}
+
+	@Test
+	public void testValeurNulleJUnit() {
+		// On implémente un premier truc
+		try {
+			StringUtils onTeste = new StringUtils();
+			CharSequence premierTruc = null;
+			CharSequence deuxiemeTruc = null;
+			Integer valeurObtenue = onTeste.levenshteinDistance(premierTruc, deuxiemeTruc);
+			Assert.assertNotNull(valeurObtenue);
+
+		} catch (NullPointerException e) {
+			System.err.println("Attention valeur nulle (deux valeurs nulles)");
+		}
+
+		// Assert.assertEquals(2, valeurObtenue.intValue());
+
+		// Assert.assertNull(valeurObtenue);
+
+	}
+
+	@Test
+	public void testValeurSemiNulleJUnit() {
+		// On implémente un premier truc
+		try {
+			StringUtils onTeste = new StringUtils();
+			CharSequence premierTruc = "Chaton";
+			CharSequence deuxiemeTruc = null;
+			Integer valeurObtenue = onTeste.levenshteinDistance(premierTruc, deuxiemeTruc);
+			Assert.assertNotNull(valeurObtenue);
+
+		} catch (NullPointerException e) {
+			System.err.println("Attention valeur nulle (au moins une des valeur est nulle)");
+		}
+
+		// Assert.assertEquals(2, valeurObtenue.intValue());
+
+		// Assert.assertNull(valeurObtenue);
+
+	}
+
+	@Test
+	public void testEgal2JUnit() {
+		// On implémente un premier truc
+		try {
+			StringUtils onTeste = new StringUtils();
+			CharSequence premierTruc = "Chaton";
+			CharSequence deuxiemeTruc = "Charbon";
+			Integer valeurObtenue = onTeste.levenshteinDistance(premierTruc, deuxiemeTruc);
+			Assert.assertNotNull(valeurObtenue);
+			Assert.assertEquals(2, valeurObtenue.intValue());
 
 		} catch (NullPointerException e) {
 			System.err.println("Attention valeur nulle");
